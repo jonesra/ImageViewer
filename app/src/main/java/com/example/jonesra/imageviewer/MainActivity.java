9cebbf7ed984b532c5108e6154e1851cdcf2ee4c
jonesra <jonesra1444@gmail.com>
2016-06-15 20:33:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jonesra/imageviewer/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.jonesra.imageviewer;
+
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //Define array of images consisting of the countries
+    int[] imgnames={R.drawable.bar1, R.drawable.bar2, R.drawable.bar3, R.drawable.bar4, R.drawable.bar5};
+
+    String[] imgdetails={"bar1", "bar2", "bar3", "bar4", "bar5"};
+
+    ListView currlv;
+   // int pos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // final ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+        final ImageView imageView2 = (ImageView) findViewById(R.id.imageView1);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
